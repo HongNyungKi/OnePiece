076ef18b29d175a804cd1249a6551c59d9da67b1
HongNyungKi <hnk2017@naver.com>
2020-06-22 04:31:56 +0900

    [Add]section animation(recent)

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -261,61 +261,13 @@ $(function () {
   }); //--------------------------------------
   //-------amimation------
 
-  $(".first").on("click", function () {
-    $(".first").addClass("active");
-    $(".second").removeClass("active");
-    $(".third").removeClass("active");
-    $(".fourth").removeClass("active");
-    $("#line").addClass("one");
-    $("#line").removeClass("two");
-    $("#line").removeClass("three");
-    $("#line").removeClass("four");
-    $("#shoppingCart").addClass("active");
-    $("#pay").removeClass("active");
-    $("#like").removeClass("active");
-    $("#question").removeClass("active");
+  $(".first").click(function () {
+    $("#recent").addClass("active");
+    $("#previous").removeClass("active");
   });
-  $(".second").on("click", function () {
-    $(".first").removeClass("active");
-    $(".second").addClass("active");
-    $(".third").removeClass("active");
-    $(".fourth").removeClass("active");
-    $("#line").removeClass("one");
-    $("#line").addClass("two");
-    $("#line").removeClass("three");
-    $("#line").removeClass("four");
-    $("#shoppingCart").removeClass("active");
-    $("#pay").addClass("active");
-    $("#like").removeClass("active");
-    $("#question").removeClass("active");
-  });
-  $(".third").on("click", function () {
-    $(".first").removeClass("active");
-    $(".second").removeClass("active");
-    $(".third").addClass("active");
-    $(".fourth").removeClass("active");
-    $("#line").removeClass("one");
-    $("#line").removeClass("two");
-    $("#line").addClass("three");
-    $("#line").removeClass("four");
-    $("#shoppingCart").removeClass("active");
-    $("#pay").removeClass("active");
-    $("#like").addClass("active");
-    $("#question").removeClass("active");
-  });
-  $(".fourth").on("click", function () {
-    $(".first").removeClass("active");
-    $(".second").removeClass("active");
-    $(".third").removeClass("active");
-    $(".fourth").addClass("active");
-    $("#line").removeClass("one");
-    $("#line").removeClass("two");
-    $("#line").removeClass("three");
-    $("#line").addClass("four");
-    $("#shoppingCart").removeClass("active");
-    $("#pay").removeClass("active");
-    $("#like").removeClass("active");
-    $("#question").addClass("active");
+  $(".second").click(function () {
+    $("#previous").addClass("active");
+    $("#recent").removeClass("active");
   }); //-----------------
 });
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -346,7 +298,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50222" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49507" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
